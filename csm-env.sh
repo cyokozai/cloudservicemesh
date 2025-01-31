5c970af72f8d6efa05cc742b18291096e04a1353
cyokozai <yuu4suke27@outlook.jp>
2025-01-31 16:40:16 +0900

    csm-env.shを修正し、環境変数の入力をユーザーから受け取るように変更; 直接の値をエクスポートするのを削除

--- csm-env.sh
+++ csm-env.sh
@@ -2,16 +2,16 @@
 # Initialize environment variables
 echo "Initializing environment variables..."
 
-# ------ DO NOT COMMIT YOUR CREDENTIALS ------ #
+read -p "Enter FLEET_PROJECT_ID:   " FLEET_PROJECT_ID
+read -p "Enter CLUSTER_PROJECT_ID: " CLUSTER_PROJECT_ID
+read -p "Enter NETWORK_PROJECT_ID: " NETWORK_PROJECT_ID
+read -p "Enter REGION:             " REGION
 
-export FLEET_PROJECT_ID=sreake-intern
-export CLUSTER_PROJECT_ID=yinoue-csm-bookinfo
-export NETWORK_PROJECT_ID=yinoue-csm-vpc
-
-# ------ DO NOT COMMIT YOUR CREDENTIALS ------ #
-
-export REGION=asia-northeast1
-export LOCATION=asia-northeast1-a
+export FLEET_PROJECT_ID=$FLEET_PROJECT_ID
+export CLUSTER_PROJECT_ID=$CLUSTER_PROJECT_ID
+export NETWORK_PROJECT_ID=$NETWORK_PROJECT_ID
+export REGION=$REGION
+export LOCATION=${REGION}-a
 export CONTEXT="gke_${FLEET_PROJECT_ID}_${LOCATION}_${CLUSTER_PROJECT_ID}"
 
 echo "Initializing asmctl configuration..."
@@ -22,14 +22,14 @@ export OUTPUT_DIR=output
 echo "Environment variables initialized."
 
 cat <<EOF > ./asm/asmcli.env
-export FLEET_PROJECT_ID=$FLEET_PROJECT_ID
-export CLUSTER_PROJECT_ID=$CLUSTER_PROJECT_ID
-export NETWORK_PROJECT_ID=$NETWORK_PROJECT_ID
-export REGION=$REGION
-export LOCATION=$LOCATION
-export CONTEXT=$CONTEXT
-export HPATH=$HPATH
-export OUTPUT_DIR=$OUTPUT_DIR
+FLEET_PROJECT_ID=$FLEET_PROJECT_ID
+CLUSTER_PROJECT_ID=$CLUSTER_PROJECT_ID
+NETWORK_PROJECT_ID=$NETWORK_PROJECT_ID
+REGION=$REGION
+LOCATION=$LOCATION
+CONTEXT=$CONTEXT
+HPATH=$HPATH
+OUTPUT_DIR=$OUTPUT_DIR
 EOF
 
 echo "./asm/asmcli.env file created."
