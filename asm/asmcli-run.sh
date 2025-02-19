552f7e1b1b5af16f90ed11d02daeb577996bd578
cyokozai <yuu4suke27@outlook.jp>
2025-02-19 22:15:05 +0900

    asmcli-run.shでAnthos Service Meshのインストールと検証のロジックを修正し、エラーハンドリングを追加

--- asm/asmcli-run.sh
+++ asm/asmcli-run.sh
@@ -9,18 +9,6 @@ gcloud config set project $FLEET_PROJECT_ID
 # Gwt the project credentials
 gcloud container clusters get-credentials $CLUSTER_PROJECT_ID --location $LOCATION --project $FLEET_PROJECT_ID
 
-# Validate Anthos Service Mesh
-echo "Validating Anthos Service Mesh"
-
-./asmcli validate \
-    --project_id $FLEET_PROJECT_ID \
-    --cluster_name $CLUSTER_PROJECT_ID \
-    --cluster_location $LOCATION \
-    --fleet_id $FLEET_PROJECT_ID \
-    --output_dir $HPATH/$OUTPUT_DIR
-
-echo "Validation complete"
-
 # Install Anthos Service Mesh
 echo "Installing Anthos Service Mesh"
 
@@ -33,12 +21,35 @@ echo "Installing Anthos Service Mesh"
     --enable_all \
     --ca mesh_ca
 
-echo "-------------------------"
-echo "ls ./"
-ls ./
-echo "-------------------------"
-echo "ls .$OUTPUT_DIR"
-ls .$OUTPUT_DIR
-echo "-------------------------"
+if [ $? -ne 0 ]; then
+    echo "Failed to install Anthos Service Mesh"
+    exit 1
+else
+    echo "Anthos Service Mesh installed successfully"
+fi
+
+# Validate Anthos Service Mesh
+echo "Validating Anthos Service Mesh"
+
+./asmcli validate \
+    --project_id $FLEET_PROJECT_ID \
+    --cluster_name $CLUSTER_PROJECT_ID \
+    --cluster_location $LOCATION \
+    --fleet_id $FLEET_PROJECT_ID \
+    --output_dir $HPATH/$OUTPUT_DIR
+
+if [ $? -ne 0 ]; then
+    echo "Failed to validate Anthos Service Mesh"
+    exit 1
+else
+    echo "Anthos Service Mesh validated successfully"
+    echo "-------------------------"
+    echo "ls ./"
+    ls ./
+    echo "-------------------------"
+    echo "ls .$OUTPUT_DIR"
+    ls .$OUTPUT_DIR
+    echo "-------------------------"
+fi
 
 exit 0
