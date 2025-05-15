1271fe842f6e792d9133cc9770ef05d83f9aaffe
cyokozai <yuu4suke27@outlook.jp>
2025-05-15 18:01:24 +0900

    asmcli-run.shのインストールセクションを整理し、重複した引数を削除

--- asm/asmcli-run.sh
+++ asm/asmcli-run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+
 # Authenticate with gcloud
 gcloud auth login --no-launch-browser
+
 
 # Set the project ID
 gcloud config set project $FLEET_PROJECT_ID
 
+
 # Get the project credentials
 gcloud container clusters get-credentials $CLUSTER_PROJECT_ID --location $REGION --project $FLEET_PROJECT_ID
 
+
+
 # Install Anthos Service Mesh
 echo "Installing Anthos Service Mesh"
+
 
 ./asmcli install \
     --project_id "$FLEET_PROJECT_ID" \
@@ -21,16 +27,6 @@ echo "Installing Anthos Service Mesh"
     --enable_all \
     --ca mesh_ca
 
-  --project_id $FLEET_PROJECT_ID \
-  --cluster_name $CLUSTER_PROJECT_ID \
-  --cluster_location $REGION \
-  --fleet_id $FLEET_PROJECT_ID \
-  --output_dir ./asm-output \
-  --enable_all \
-  --ca mesh_ca \
-  --channel rapid
-
-  
 
 if [ $? -ne 0 ]; then
     echo "Failed to install Anthos Service Mesh"
@@ -39,8 +35,11 @@ else
     echo "Anthos Service Mesh installed successfully"
 fi
 
+
+
 # Validate Anthos Service Mesh
 echo "Validating Anthos Service Mesh"
+
 
 ./asmcli validate \
     --project_id "$FLEET_PROJECT_ID" \
@@ -48,6 +47,7 @@ echo "Validating Anthos Service Mesh"
     --cluster_location "$REGION" \
     --fleet_id "$FLEET_PROJECT_ID" \
     --output_dir "${HPATH}${OUTPUT_DIR}"
+
 
 if [ $? -ne 0 ]; then
     echo "Failed to validate Anthos Service Mesh"
@@ -62,5 +62,6 @@ else
     ls .$OUTPUT_DIR
     echo "-------------------------"
 fi
+
 
 exit 0
